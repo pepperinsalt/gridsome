492a66d272f3a77723e97a19db80aba7796938f4
pepperinsalt <kacyedwardculpepper@gmail.com>
2026-05-10 22:53:18 -0600

    Add Page wrapper component

---
+++ src/components/Page.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok" class="w-full">
+    <component
+      v-for="item in blok.body"
+      :key="item._uid"
+      :blok="item"
+      :is="item.component"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
